9da87a535159c804cd5e110cc297841c256a33cc
MSFauzan505 <mshafauzan01@gmail.com>
2025-01-10 20:40:37 +0700

    ubah ppage kandidat dari bootstrap native jadi stisla

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -2,56 +2,117 @@
 
 <?= $this->section('content') ?>
 <!-- chart  -->
+
+<!-- Kandidat Paling Populer -->
+<div class="col-12 mt-4">
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+                <!-- Statistik -->
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Statistik kandidat</h4>
+                            <div class="card-header-action">
+                                <a href="#" class="btn active">PPLG</a>
+                                <a href="#" class="btn">AKL</a>
+                                <a href="#" class="btn">TJKT</a>
+                                <a href="#" class="btn">MPLB</a>
+                                <a href="#" class="btn">ULP</a>
+                                <a href="#" class="btn">PM</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col mb-4 mb-lg-0 text-center">
+                    <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle"
+                        style="width: 75px; height: 75px;">
+                    <div class="mt-2 font-weight-bold">ujaa</div>
+                    <div class="text-medium font-weight-bold"><span class="text-primary">76<i class="fas fa-percent"></i></span>
+                        </div>
+                </div>
+                <div class="col mb-4 mb-lg-0 text-center">
+                    <img alt="image" src="assets/img/avatar/avatar-2.png" class="rounded-circle"
+                        style="width: 75px; height: 75px;">
+                    <div class="mt-2 font-weight-bold">Kandadats</div>
+                    <div class="text-medium font-weight-bold"><span class="text-primary">23<i class="fas fa-percent"></i></span>
+                        </div>
+                </div>
+                <div class="col mb-4 mb-lg-0 text-center">
+                    <img alt="image" src="assets/img/avatar/avatar-3.png" class="rounded-circle"
+                        style="width: 75px; height: 75px;">
+                    <div class="mt-2 font-weight-bold">Kandadats</div>
+                    <div class="text-medium font-weight-bold"><span class="text-primary">23<i class="fas fa-percent"></i></span>
+                        </div>
+                </div>
+                <div class="col mb-4 mb-lg-0 text-center">
+                    <img alt="image" src="assets/img/avatar/avatar-4.png" class="rounded-circle"
+                        style="width: 75px; height: 75px;">
+                    <div class="mt-2 font-weight-bold">Kandadats</div>
+                    <div class="text-medium font-weight-bold"><span class="text-primary">23<i class="fas fa-percent"></i></span>
+                        </div>
+                </div>
+                <div class="col mb-4 mb-lg-0 text-center">
+                    <img alt="image" src="assets/img/avatar/avatar-5.png" class="rounded-circle"
+                        style="width: 75px; height: 75px;">
+                    <div class="mt-2 font-weight-bold">Kandadats</div>
+                    <div class="text-medium font-weight-bold"><span class="text-primary">23<i class="fas fa-percent"></i></span>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <section class="section">
     <div class="row">
         <div class="col-lg-4 col-md-4 col-sm-12">
             <div class="card card-statistic-2">
                 <div class="card-stats">
-                    <div class="card-stats-title">Order Statistics -
+                    <div class="card-stats-title">Data Kelas -
                         <div class="dropdown d-inline">
                             <a class="font-weight-600 dropdown-toggle" data-toggle="dropdown" href="#"
-                                id="orders-month">August</a>
+                                id="orders-month">Pilih Kelas</a>
                             <ul class="dropdown-menu dropdown-menu-sm">
-                                <li class="dropdown-title">Select Month</li>
-                                <li><a href="#" class="dropdown-item">January</a></li>
-                                <li><a href="#" class="dropdown-item">February</a></li>
-                                <li><a href="#" class="dropdown-item">March</a></li>
-                                <li><a href="#" class="dropdown-item">April</a></li>
-                                <li><a href="#" class="dropdown-item">May</a></li>
-                                <li><a href="#" class="dropdown-item">June</a></li>
-                                <li><a href="#" class="dropdown-item">July</a></li>
-                                <li><a href="#" class="dropdown-item ">August</a></li>
-                                <li><a href="#" class="dropdown-item">September</a></li>
-                                <li><a href="#" class="dropdown-item">October</a></li>
-                                <li><a href="#" class="dropdown-item">November</a></li>
-                                <li><a href="#" class="dropdown-item">December</a></li>
+                                <li class="dropdown-title">Select Class</li>
+                                <li><a href="#" class="dropdown-item active">Semua</a></li>
+                                <li><a href="#" class="dropdown-item">X PPLG 1</a></li>
+                                <li><a href="#" class="dropdown-item">X PPLG 2</a></li>
+                                <li><a href="#" class="dropdown-item">X PPLG 3</a></li>
+                                <li><a href="#" class="dropdown-item">XI PPLG 1</a></li>
+                                <li><a href="#" class="dropdown-item">XI PPLG 2</a></li>
+                                <li><a href="#" class="dropdown-item">XI PPLG 3</a></li>
+                                <li><a href="#" class="dropdown-item">XII PPLG 1</a></li>
+                                <li><a href="#" class="dropdown-item ">XII PPLG 2</a></li>
+                                <li><a href="#" class="dropdown-item">XII PPLG 3</a></li>
+                                <li><a href="#" class="dropdown-item">X AKL 1</a></li>
+                                <li><a href="#" class="dropdown-item">X AKL 2</a></li>
+                                <li><a href="#" class="dropdown-item">X AKL 3</a></li>
                             </ul>
                         </div>
                     </div>
                     <div class="card-stats-items">
                         <div class="card-stats-item">
                             <div class="card-stats-item-count">24</div>
-                            <div class="card-stats-item-label">Pending</div>
-                        </div>
-                        <div class="card-stats-item">
-                            <div class="card-stats-item-count">12</div>
-                            <div class="card-stats-item-label">Shipping</div>
+                            <div class="card-stats-item-label">Belum Vote</div>
                         </div>
                         <div class="card-stats-item">
                             <div class="card-stats-item-count">23</div>
-                            <div class="card-stats-item-label">Completed</div>
+                            <div class="card-stats-item-label">Sudah Vote</div>
                         </div>
                     </div>
                 </div>
                 <div class="card-icon shadow-primary bg-primary">
-                    <i class="fas fa-archive"></i>
+                    <i class="fas fa-user"></i>
                 </div>
                 <div class="card-wrap">
                     <div class="card-header">
-                        <h4>Total Orders</h4>
+                        <h4>Total yang Terdaftar</h4>
                     </div>
                     <div class="card-body">
-                        59
+                        200
                     </div>
                 </div>
             </div>
@@ -116,11 +177,9 @@
                 </div>
             </div>
         </div>
-    </div>
+        >
 </section>
 <section class="section">
-
-
     <div class="section-body">
         <h2 class="section-title">Chart.js</h2>
         <p class="section-lead">
@@ -174,95 +233,11 @@
                 </div>
             </div>
         </div>
-       <div class="row">
-            <!-- Statistik -->
-        <div class="col-12">
-            <div class="card">
-                <div class="card-header">
-                    <h4>Statistik kandidat</h4>
-                    <div class="card-header-action">
-                        <a href="#" class="btn active">PPLG</a>
-                        <a href="#" class="btn">AKL</a>
-                        <a href="#" class="btn">TJKT</a>
-                        <a href="#" class="btn">MPLB</a>
-                        <a href="#" class="btn">ULP</a>
-                        <a href="#" class="btn">PM</a>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <canvas id="myChart2" height="180"></canvas>
-                    <div class="statistic-details mt-1">
-                        <div class="statistic-details-item">
-                            <div class="text-small text-muted"><span class="text-primary"><i class="fas fa-caret-up"></i></span> 7%</div>
-                            <div class="detail-value">Ujaaa</div>
-                            <div class="detail-name">bang uja</div>
-                        </div>
-                        <div class="statistic-details-item">
-                            <div class="text-small text-muted"><span class="text-danger"><i class="fas fa-caret-down"></i></span> 23%</div>
-                            <div class="detail-value">Djawa</div>
-                            <div class="detail-name">el jawir</div>
-                        </div>
-                        <div class="statistic-details-item">
-                            <div class="text-small text-muted"><span class="text-primary"><i class="fas fa-caret-up"></i></span> 9%</div>
-                            <div class="detail-value">Kandidut</div>
-                            <div class="detail-name">kandiduted</div>
-                        </div>
-                        <div class="statistic-details-item">
-                            <div class="text-small text-muted"><span class="text-primary"><i class="fas fa-caret-up"></i></span> 19%</div>
-                            <div class="detail-value">Kandadat</div>
-                            <div class="detail-name">kandadatss</div>
-                        </div>
-                        <div class="statistic-details-item">
-                            <div class="text-small text-muted"><span class="text-primary"><i class="fas fa-caret-up"></i></span> 19%</div>
-                            <div class="detail-value">Kandudut</div>
-                            <div class="detail-name">kandudutss</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Kandidat Paling Populer -->
-        <div class="col-12 mt-4">
-            <div class="card">
-                <div class="card-header">
-                    <h4>Kandidat Paling Populer</h4>
-                </div>
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col mb-4 mb-lg-0 text-center">
-                        <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle" style="width: 75px; height: 75px;">
-                            <div class="mt-2 font-weight-bold">ujaa</div>
-                            <div class="text-small text-muted"><span class="text-primary"><i class="fas fa-caret-up"></i></span> 48%</div>
-                        </div>
-                        <div class="col mb-4 mb-lg-0 text-center">
-                        <img alt="image" src="assets/img/avatar/avatar-2.png" class="rounded-circle" style="width: 75px; height: 75px;">
-                            <div class="mt-2 font-weight-bold">Kandadats</div>
-                            <div class="text-small text-muted"><span class="text-primary"><i class="fas fa-caret-up"></i></span> 26%</div>
-                        </div>
-                        <div class="col mb-4 mb-lg-0 text-center">
-                        <img alt="image" src="assets/img/avatar/avatar-3.png" class="rounded-circle" style="width: 75px; height: 75px;">
-                            <div class="mt-2 font-weight-bold">Djawa</div>
-                            <div class="text-small text-muted"><span class="text-danger"><i class="fas fa-caret-down"></i></span> 14%</div>
-                        </div>
-                        <div class="col mb-4 mb-lg-0 text-center">
-                        <img alt="image" src="assets/img/avatar/avatar-4.png" class="rounded-circle" style="width: 75px; height: 75px;">
-                            <div class="mt-2 font-weight-bold">Opera</div>
-                            <div class="text-small text-muted">7%</div>
-                        </div>
-                        <div class="col mb-4 mb-lg-0 text-center">
-                        <img alt="image" src="assets/img/avatar/avatar-5.png" class="rounded-circle" style="width: 75px; height: 75px;">
-                            <div class="mt-2 font-weight-bold">IE</div>
-                            <div class="text-small text-muted"><span class="text-primary"><i class="fas fa-caret-up"></i></span> 5%</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </div>
+
     </div>
 </section>
-
 </div>
+
 
 <?= $this->endSection() ?>
 
--- app/Views/poll/main.php
+++ app/Views/poll/main.php
@@ -3,111 +3,157 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet">
-    <title>Document</title>
-</head>
-<style>
-    * {
-        padding: 0;
-        margin: 0;
-        box-sizing: border-box;
-        font-family: 'Poppins', sans-serif;
-    }
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>Layout &rsaquo; Top Navigation &mdash; Stisla</title>
 
-    /* Atur posisi untuk ul */
-    .dropdown-list {
-        position: absolute;
-        bottom: -70px;
-        /* Geser ke bawah tombol */
-        right: 0;
-        background-color: white;
-        /* Tambahkan latar belakang */
-        border: 1px solid #ddd;
-        /* Tambahkan border */
-        border-radius: 5px;
-        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
-        display: none;
-        /* Sembunyikan awalnya */
-        z-index: 10;
-        min-width: 150px;
-        /* Lebar minimum dropdown */
-    }
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="assets/modules/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/modules/fontawesome/css/all.min.css">
 
-    .dropdown-list {
-        margin-left: .5rem;
-        list-style: none;
-        padding: 10px;
-        text-align: left;
-        cursor: pointer;
-    }
+    <!-- CSS Libraries -->
 
-    .dropdown-list:hover {
-        background-color: #f0f0f0;
-        /* Efek hover */
-    }
-
-    .dropdown-container {
-        position: relative;
-        /* Wajib untuk kontrol posisi */
-    }
-</style>
-
-<body>
-    <!-- Navigation Menu -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
-        <div class="container-fluid px-4">
-            <a class="navbar-brand d-flex align-items-center gap-2" href="#">
-                <img src="<?= base_url() ?>assets/img/logo.png" alt="Logo" class="rounded-circle" style="height: 50px;">
-                <span class="d-none d-sm-block fw-bold">SMKN 2 KUNINGAN</span>
-            </a>
-            <form class="d-flex ms-auto align-items-center">
-                <div class="dropdown-container">
-                    <button id="dropdownButton" class="btn btn-outline-light rounded-pill" type="button">
-                        <img id="profileBtn" src="<?= base_url() ?>assets/img/profile.svg" alt="search-icon"
-                            style="width: 20px;">
-                    </button>
-                    <ul id="dropdownList" class="dropdown-list">
-                        <li>Logout</li>
-                    </ul>
-                </div>
-            </form>
-        </div>
-    </nav>
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/components.css">
+    <!-- Start GA -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
     <script>
-        const dropdownButton = document.getElementById('dropdownButton');
-        const dropdownList = document.getElementById('dropdownList');
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
 
-        dropdownButton.addEventListener('click', () => {
-            // Toggle visibility
-            const isVisible = dropdownList.style.display === 'block';
-            dropdownList.style.display = isVisible ? 'none' : 'block';
-        });
-
-        // Klik di luar dropdown untuk menutupnya
-        document.addEventListener('click', (event) => {
-            if (!dropdownButton.contains(event.target) && !dropdownList.contains(event.target)) {
-                dropdownList.style.display = 'none';
-            }
-        });
+        gtag('config', 'UA-94034622-3');
     </script>
+    <!-- /END GA -->
+</head>
 
-    <?= $this->renderSection('content') ?>
+<body class="layout-3">
+    <div id="app">
+        <div class="main-wrapper container">
+            <div class="navbar-bg"></div>
+            <nav class="navbar navbar-expand-lg main-navbar">
+                <img src="<?base_url()?>assets/" alt="">
+                <a href="index.html" class="navbar-brand sidebar-gone-hide">SMKN 2 Kuningan</a>
+                <a href="#" class="nav-link sidebar-gone-show m-3" data-toggle="sidebar">SMKN 2 Kuningan</a>
 
-    <!-- Bootstrap Scripts -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+                <form class="form-inline ml-auto">
+                    <ul class="navbar-nav ">
+                        <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i
+                                    class="fas fa-search"></i></a></li>
+                    </ul>
+                    <div class="search-element">
+                        <input class="form-control" type="search" placeholder="Cari Kandidat" aria-label="Search"
+                            data-width="250">
+                        <button class="btn" type="submit"><i class="fas fa-search"></i></button>
+                    </div>
+                </form>
+                <ul class="navbar-nav navbar-right">
+                    <li class="dropdown"><a href="#" data-toggle="dropdown"
+                            class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+                            <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
+                            <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right">
+                            
+                            <a href="#" class="dropdown-item has-icon text-danger">
+                                <i class="fas fa-sign-out-alt"></i> Logout
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+
+
+            <!-- Main Content -->
+            <div class="main-content">
+                <section class="section bg-primary rounded p-1 mb-3">
+                    <h1 class="text-center text-white text-uppercase mt-2">Para Kandidat</h1>
+                </section>
+
+                <!-- card kandidat -->
+                <div class="row">
+                    <div class="col-12 col-sm-6 col-md-6 col-lg-4">
+                        <article class="article ">
+                            <div class="article-header rounded">
+                                <div class="article-image bg-secondary"
+                                    data-background="<?= base_url() ?>assets/img/news/img08.jpg">
+                                </div>
+                            </div>
+                            <div class="article-details">
+                                <h3 class="text-center font-weight-bold">Gustavo Fring</h3>
+                                <p class="text-center text-muted">Kandidat 1</p>
+                                <div class="article-cta">
+                                    <button type="button" class="btn btn-primary" data-toggle="modal"
+                                        data-target="#exampleModal">
+                                        Vote
+                                    </button>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl"> <!-- Kelas modal-xl untuk modal large -->
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <!-- Bagian kiri untuk gambar -->
+                    <div class="col-3">
+                        <img src="<?= base_url() ?>assets/img/avatar/avatar-1.png" class="img-fluid" alt="kandidat">
+                    </div>
+                    <!-- Bagian kanan untuk detail -->
+                    <div class="col-9">
+                        <h3>Gustavo Fring</h3>
+                        <div>
+                            <p><strong>Visi:</strong> 
+                                <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum quis fuga eos facere pariatur laudantium deleniti, accusamus modi doloremque architecto, minus enim sed aliquam quasi laborum iure id accusantium temporibus.</span>
+                            </p>
+                            <p><strong>Misi:</strong>
+                                <ul>
+                                    <li>1. ambasing</li>
+                                    <li>2. ambasing</li>
+                                    <li>3. ambasing</li>
+                                    <li>4. ambasing</li>
+                                    <li>5. ambasing</li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+    <!-- General JS Scripts -->
     <script src="<?= base_url() ?>assets/modules/jquery.min.js"></script>
     <script src="<?= base_url() ?>assets/modules/popper.js"></script>
     <script src="<?= base_url() ?>assets/modules/tooltip.js"></script>
+    <script src="<?= base_url() ?>assets/modules/bootstrap/js/bootstrap.min.js"></script>
     <script src="<?= base_url() ?>assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
     <script src="<?= base_url() ?>assets/modules/moment.min.js"></script>
     <script src="<?= base_url() ?>assets/js/stisla.js"></script>
-    <?= $this->renderSection("script") ?>
+
     <!-- JS Libraies -->
 
     <!-- Page Specific JS File -->
