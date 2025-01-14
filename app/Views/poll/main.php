fe5f2ad84025cd10f2a6f84536a4c3971ffb45dc
MSFauzan505 <mshafauzan01@gmail.com>
2025-01-14 10:28:44 +0700

    edit sedikit di bagian poll

--- app/Views/poll/main.php
+++ app/Views/poll/main.php
@@ -3,27 +3,33 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
     <title>Layout &rsaquo; Top Navigation &mdash; Stisla</title>
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="assets/modules/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/modules/fontawesome/css/all.min.css">
 
-    <!-- CSS Libraries -->
-
     <!-- Template CSS -->
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/components.css">
     <style>
-     
+        .modal-xl-custom {
+            max-width: 90%;
+            width: 90%;
+        }
 
-     
+        .btn-custom {
+            width: 8rem;
+        }
 
-
-        /* Optional: Add hover effect for smooth image scaling */
+        .large {
+            width: 8rem;
+            height: 8rem;
+        }
     </style>
-    <!-- Start GA -->
+
+    <!-- Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
@@ -32,168 +38,137 @@
             dataLayer.push(arguments);
         }
         gtag('js', new Date());
-
         gtag('config', 'UA-94034622-3');
     </script>
-    <!-- /END GA -->
 </head>
 
 <body class="layout-3">
     <div id="app">
         <div class="main-wrapper container">
             <div class="navbar-bg"></div>
+
+            <!-- Main Navbar -->
             <nav class="navbar navbar-expand-lg main-navbar">
-                <img src="<? base_url() ?>assets/" alt="">
                 <a href="index.html" class="navbar-brand sidebar-gone-hide">SMKN 2 Kuningan</a>
-                <a href="#" class="nav-link sidebar-gone-show m-3" data-toggle="sidebar">SMKN 2 Kuningan</a>
+                <a href="#" class="nav-link sidebar-gone-show m-3" data-toggle="sidebar"> <i
+                        class="fas fa-bars large"></i></a>
 
                 <form class="form-inline ml-auto">
-                    <ul class="navbar-nav ">
-                        <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i
-                                    class="fas fa-search"></i></a></li>
-                    </ul>
                     <div class="search-element">
                         <input class="form-control" type="search" placeholder="Cari Kandidat" aria-label="Search"
                             data-width="250">
                         <button class="btn" type="submit"><i class="fas fa-search"></i></button>
                     </div>
                 </form>
+
                 <ul class="navbar-nav navbar-right">
-                    <li class="dropdown"><a href="#" data-toggle="dropdown"
-                            class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+                    <li class="dropdown">
+                        <a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
                             <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
                             <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right">
-
-                            <a href="#" class="dropdown-item has-icon text-danger">
-                                <i class="fas fa-sign-out-alt"></i> Logout
+                            <a href="#" class="dropdown-item has-icon text-primary">
+                                <i class="fas fa-sign-out-alt"></i> Profile
                             </a>
                         </div>
                     </li>
                 </ul>
             </nav>
 
+            <!-- Secondary Navbar -->
             <nav class="navbar navbar-secondary navbar-expand-lg">
                 <div class="container">
                     <ul class="navbar-nav">
-                        
                         <li class="nav-item active">
-                            <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Home</span></a>
+                            <a class="nav-link" href="index.html"><i class="fas fa-fire"></i> <span>Dashboard</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fas fa-users"></i> <span>Para Kandidat</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fas fa-poll"></i> <span>Voting</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fas fa-cogs"></i> <span>Pengaturan</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-danger" href="#"><i class="fas fa-sign-out-alt"></i>
+                                <span>Logout</span></a>
                         </li>
                     </ul>
                 </div>
             </nav>
 
-
             <!-- Main Content -->
             <div class="main-content">
-                <!-- <section class="section bg-primary rounded p-1 mb-3">
-                    <h1 class="text-center text-white text-uppercase mt-2">Para Kandidat</h1>
-                </section> -->
-
-                <section class="section">
+                <section class="section mt-3">
                     <div class="section-header">
-                        <h1>Top Navigation</h1>
+                        <h1>Para Kandidat</h1>
                         <div class="section-header-breadcrumb">
-                            <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-                            <div class="breadcrumb-item"><a href="#">Layout</a></div>
-                            <div class="breadcrumb-item">Top Navigation</div>
+                            <div class="breadcrumb-item"><a href="#">Para Kandidat</a></div>
+                            <div class="breadcrumb-item"><a href="#">Voting</a></div>
                         </div>
                     </div>
 
                     <div class="section-body">
-
-                        <div class="row justify-content-center ">
-                            <div class="col-6 col-sm-6 col-md-6 col-lg-4">
-                                <article class="article ">
+                        <div class="row justify-content-center">
+                            <div class="col-12 col-sm-6 col-md-6 col-lg-4">
+                                <article class="article">
                                     <div class="article-header rounded">
-                                        <div class="article-image bg-secondary"
-                                            data-background="https://picsum.photos/400/300">
+                                        <div class="article-image bg-secondary img-fluid"
+                                            style="background-image: url('https://picsum.photos/400/300');">
                                         </div>
                                     </div>
                                     <div class="article-details">
                                         <h3 class="text-center font-weight-bold">Gustavo Fring</h3>
                                         <p class="text-center text-muted">Kandidat 1</p>
                                         <div class="article-cta">
-                                            <button type="button" class="btn btn-primary" data-toggle="modal"
+                                            <button type="button" class="btn btn-primary btn-custom" data-toggle="modal"
                                                 data-target="#exampleModal">
-                                                Vote
+                                                Visi & Misi
                                             </button>
                                         </div>
                                     </div>
                                 </article>
-
                             </div>
-                            <div class="col-6 col-sm-6 col-md-6 col-lg-4">
-                                <article class="article ">
-                                    <div class="article-header rounded">
-                                        <div class="article-image bg-secondary"
-                                            data-background="https://picsum.photos/400/300">
-                                        </div>
-                                    </div>
-                                    <div class="article-details">
-                                        <h3 class="text-center font-weight-bold">Gustavo Fring</h3>
-                                        <p class="text-center text-muted">Kandidat 1</p>
-                                        <div class="article-cta">
-                                            <button type="button" class="btn btn-primary" data-toggle="modal"
-                                                data-target="#exampleModal">
-                                                Vote
-                                            </button>
-                                        </div>
-                                    </div>
-                                </article>
-
-                            </div>
-                            
                         </div>
                     </div>
                 </section>
-
-                <!-- card kandidat -->
-
-
             </div>
         </div>
     </div>
 
-
-
-
     <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-xl">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Detail Kandidat</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div class="container-fluid p-0">
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-xl modal-xl-custom">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Detail Kandidat</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
                     <div class="row">
-                        <!-- Bagian kiri untuk gambar -->
                         <div class="col-lg-4 col-md-12 mb-4">
-                            <img src="<?= base_url() ?>assets/img/avatar/avatar-1.png" class="img-fluid rounded mx-auto d-block" alt="kandidat" style="max-height: 300px;">
+                            <img src="assets/img/avatar/avatar-1.png" class="img-fluid rounded mx-auto d-block"
+                                alt="kandidat" style="max-height: 300px;">
                         </div>
-                        <!-- Bagian kanan untuk detail -->
                         <div class="col-lg-8 col-md-12">
                             <h3 class="mb-4 text-center text-lg-left">Gustavo Fring</h3>
                             <div class="card">
                                 <div class="card-body">
                                     <div class="mb-4">
                                         <h5><strong>Visi:</strong></h5>
-                                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum quis fuga eos facere pariatur laudantium deleniti, accusamus modi doloremque architecto, minus enim sed aliquam quasi laborum iure id accusantium temporibus.</p>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                                     </div>
                                     <div>
                                         <h5><strong>Misi:</strong></h5>
-                                        <ul class="pl-3">
+                                        <ul>
                                             <li>1. ambasing</li>
                                             <li>2. ambasing</li>
-                                            <li>3. ambasing</li>
-                                            <li>4. ambasing</li>
-                                            <li>5. ambasing</li>
                                         </ul>
                                     </div>
                                 </div>
@@ -201,32 +176,22 @@
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Vote Kandidat</button>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
-
 
     <!-- General JS Scripts -->
-    <script src="<?= base_url() ?>assets/modules/jquery.min.js"></script>
-    <script src="<?= base_url() ?>assets/modules/popper.js"></script>
-    <script src="<?= base_url() ?>assets/modules/tooltip.js"></script>
-    <script src="<?= base_url() ?>assets/modules/bootstrap/js/bootstrap.min.js"></script>
-    <script src="<?= base_url() ?>assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
-    <script src="<?= base_url() ?>assets/modules/moment.min.js"></script>
-    <script src="<?= base_url() ?>assets/js/stisla.js"></script>
-
-    <!-- JS Libraies -->
-
-    <!-- Page Specific JS File -->
+    <script src="assets/modules/jquery.min.js"></script>
+    <script src="assets/modules/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
+    <script src="assets/js/stisla.js"></script>
 
     <!-- Template JS File -->
-    <script src="<?= base_url() ?>assets/js/scripts.js"></script>
-    <script src="<?= base_url() ?>assets/js/custom.js"></script>
+    <script src="assets/js/scripts.js"></script>
+    <script src="assets/js/custom.js"></script>
 </body>
 
 </html>
