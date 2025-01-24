d4f507de9c90aed6341d21a4c5075c624616f47d
Rafsan <rapsanza@gmail.com>
2025-01-24 14:20:05 +0700

    fix web_config helper

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -35,7 +35,7 @@ abstract class BaseController extends Controller
      *
      * @var list<string>
      */
-    protected $helpers = ['auth', 'config'];
+    protected $helpers = ['auth', 'web_config'];
 
     /**
      * Be sure to declare properties for any property fetch you initialized.
