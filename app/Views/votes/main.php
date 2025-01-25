4c64a2df3bfc9dd835b4d5300ff3ff97d46c962e
MSFauzan505 <mshafauzan01@gmail.com>
2025-01-25 10:01:03 +0700

    ubah nickname jadi ril_pinkman

--- app/Views/votes/main.php
+++ app/Views/votes/main.php
@@ -67,7 +67,7 @@
                     <li class="dropdown">
                         <a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
                             <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
-                            <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div>
+                            <div class="d-sm-none d-lg-inline-block">ril_pinkman</div>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right">
                             <a href="#" class="dropdown-item has-icon text-primary">
